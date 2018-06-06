9ac663e38ca094aa419dcd4e5a3187ba7072b7f9
Nicci Sheets <niccisheets@aol.com>
2018-06-06 09:18:37 -0400

    first commit

---
+++ HedwigCode.rb
@@ -0,0 +1,19 @@
+with_synth :pretty_bell do
+  play 64
+  sleep 0.33
+  play [57, 69], sustain: 0.5
+  sleep 0.50
+  play 72, sustain: 0.66
+  sleep 0.33
+  play 71
+  sleep 0.33
+  play [57, 69], sustain: 0.66
+  sleep 0.33
+  play 76
+  sleep 0.33
+  play [57, 74], sustain: 1
+  sleep 1
+  play [57, 69], sustain: 1
+end
+
+
